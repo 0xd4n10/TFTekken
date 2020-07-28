e6906144f49d93bf925aa5a898ec0bdd85e0a48d
Daniel Love <danlove99@hotmail.co.uk>
2020-07-28 20:31:40 +0000

    Set a limit for how many flipped frames can be appended

--- flip_data.py
+++ flip_data.py
@@ -1,19 +1,25 @@
 import cv2 
 import numpy as np
 
-t = np.load('training_data.npy', allow_pickle=True)
+# Set limit to 25 percent because it's uncommon
+LIMIT = len(t) * 0.25
 
-new_td = []
-for i in t:
-	new_td.append(i)
-	if i[1][0] == 1.0:
-		output = np.array([0.0, 0.0, 1.0, 0.0, 0.0, 0.0])
-	elif i[1][0] == 1.0:
-		output = np.array([1.0, 0.0, 0.0, 0.0, 0.0, 0.0])
-	else:
-		output = i[1]
-	new_td.append([cv2.flip(i[0], 1), output])
-print(len(t))
-print(len(new_td))
-input()
-np.save('complete_data.npy', new_td)
+data = np.load('training_data.npy', allow_pickle=True)
+
+new_data = []
+count = 0
+
+for i in data:
+	new_data.append(i)
+	if count <= LIMIT:
+		if i[1][0] == 1.0:
+			output = np.array([0.0, 0.0, 1.0, 0.0, 0.0, 0.0])
+		elif i[1][0] == 1.0:
+			output = np.array([1.0, 0.0, 0.0, 0.0, 0.0, 0.0])
+		else:
+			output = i[1]
+		
+		new_data.append([cv2.flip(i[0], 1), output])
+	count += 1
+
+np.save('complete_data.npy', new_data)
