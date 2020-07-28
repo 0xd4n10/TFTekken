cd585f0be3f05da6ffdf3bba2a585a158055f9cb
Daniel Love <danlove99@hotmail.co.uk>
2020-07-28 20:34:21 +0000

    Update train.py

--- train.py
+++ train.py
@@ -2,20 +2,22 @@ import numpy as np
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Conv2D, Flatten#create model
 
-model = Sequential()#add model layers
+# Define model
+model = Sequential()
 model.add(Conv2D(64, kernel_size=3, activation='relu', input_shape=(120,120,1)))
 model.add(Conv2D(32, kernel_size=3, activation='relu'))
 model.add(Flatten())
 model.add(Dense(6, activation='softmax'))
 model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
 
+# Train on processed data
 train = np.load('complete_data.npy', allow_pickle=True)
 
 X = np.array([i[0] for i in train])
 X = np.expand_dims(X, axis=3)
 y = np.array([i[1] for i in train])
 
-from tensorflow.keras.callbacks import ModelCheckpoint
+from tensorflow.keras.callbacks import ModelCheckpoint # For saving weights
 checkpointer = ModelCheckpoint(filepath="weights.hdf5", verbose=1, save_best_only=True)
 model.fit(X, y, epochs=10, batch_size=10, validation_split=0.2, callbacks=[checkpointer])
 
